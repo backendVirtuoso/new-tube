b1560dd94428704d241b4f7a33b9a137ca3bdb32
backendVirtuoso <129765568+backendVirtuoso@users.noreply.github.com>
2025-06-07 18:01:44 +0900

    Mux 비디오 처리 연동

--- src/modules/studio/ui/components/studio-upload-modal.tsx
+++ src/modules/studio/ui/components/studio-upload-modal.tsx
@@ -4,6 +4,8 @@ import { toast } from "sonner";
 import { Loader2Icon, PlusIcon } from "lucide-react";
 import { Button } from "@/components/ui/button";
 import { trpc } from "@/trpc/client";
+import { ResponsiveModal } from "@/components/reponsive-modal";
+import { StudioUploader } from "./studio-uploader";
 
 export const StudioUploadModel = () => {
   const utils = trpc.useUtils();
@@ -18,9 +20,21 @@ export const StudioUploadModel = () => {
   });
 
   return (
-    <Button variant="secondary" onClick={() => create.mutate()} disabled={create.isPending}>
-      {create.isPending ? <Loader2Icon className="animate-spin" /> : <PlusIcon />}
-      Create
-    </Button>
+    <>
+      <ResponsiveModal
+        title="Upload a vidoe"
+        open={!!create.data?.url}
+        onOpenChange={() => create.reset()}
+      >
+        {create.data?.url
+          ? <StudioUploader endpoint={create.data.url} onSuccess={() => { }} />
+          : <Loader2Icon />
+        }
+      </ResponsiveModal>
+      <Button variant="secondary" onClick={() => create.mutate()} disabled={create.isPending}>
+        {create.isPending ? <Loader2Icon className="animate-spin" /> : <PlusIcon />}
+        Create
+      </Button>
+    </>
   );
 };
