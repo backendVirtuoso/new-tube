ac686bcfebf203b799171db7c7b9dd396b7d82fa
backendVirtuoso <129765568+backendVirtuoso@users.noreply.github.com>
2025-06-20 22:52:21 +0900

    댓글 시스템

--- src/trpc/routers/_app.ts
+++ src/trpc/routers/_app.ts
@@ -1,6 +1,7 @@
 import { createTRPCRouter } from "../init";
 import { studioRouter } from "@/modules/studio/server/procedures";
 import { videosRouter } from "@/modules/videos/server/procedures";
+import { commentsRouter } from "@/modules/comments/server/procedures";
 import { categoriesRouter } from "@/modules/categories/server/procedures";
 import { videoViewsRouter } from "@/modules/video-views/server/procedures";
 import { subscriptionsRouter } from "@/modules/subscriptions/server/procedures";
@@ -9,6 +10,7 @@ import { videoReactionsRouter } from "@/modules/video-reactions/server/procedure
 export const appRouter = createTRPCRouter({
   studio: studioRouter,
   videos: videosRouter,
+  comments: commentsRouter,
   categories: categoriesRouter,
   videoVidews: videoViewsRouter,
   subscriptions: subscriptionsRouter,
